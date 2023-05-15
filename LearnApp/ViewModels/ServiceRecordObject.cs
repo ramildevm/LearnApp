e0b34cb467f75e1fff0da788e750094d71a638fb
ramildevm <ramil.rahimov2003@gmail.com>
2023-05-15 23:49:48 +0300

    Добавлен просмотр всех и ближайщих записей

---
+++ LearnApp/ViewModels/ServiceRecordObject.cs
@@ -0,0 +1,20 @@
+﻿using LearnApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace LearnApp.ViewModels
+{
+    class ServiceRecordObject
+    {
+        public ServiceRecord ServiceRecord { get; set; }
+        public Service Service { get; set; }
+        public Client Client { get; set; }
+        public Brush TimeColor { get; set; }
+        public string Time { get; set; }
+        public string FIO { get; set; }
+    }
+}
--- LearnApp/Windows/AdminServiceRecordWindow.xaml.cs
+++ LearnApp/Windows/AdminServiceRecordWindow.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using LearnApp.Models;
+using LearnApp.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,9 +22,115 @@ namespace LearnApp.Windows
     /// </summary>
     public partial class AdminServiceRecordWindow : Window
     {
+        bool flagNearRecords = false;
+        private List<ServiceRecord> serviceRecords;
+        Timer myTimer;
         public AdminServiceRecordWindow()
         {
             InitializeComponent();
+            SetWindowData();
+            LoadDataSet();
+            LoadData();
+        }
+
+        private void LoadDataSet()
+        {
+            using (var db = new EntityModel())
+            {
+                if (flagNearRecords)
+                {
+                    DateTime oldTime = DateTime.Now, newTime = DateTime.Now.AddDays(1);
+                    serviceRecords = db.ServiceRecord.Where(sr => oldTime <= sr.ServiceStart && sr.ServiceStart < newTime).OrderBy(sr=>sr.ServiceStart).ToList();
+                    myTimer.Enabled = true;
+                }
+                else 
+                {
+                    serviceRecords = db.ServiceRecord.OrderBy(sr => sr.ServiceStart).ToList();
+                    myTimer.Enabled = false;
+                }
+            }
+        }
+
+        private void SetWindowData()
+        {
+            myTimer = new Timer(30000); 
+            myTimer.Elapsed += MyTimer_Elapsed;
+            myTimer.AutoReset = true;
+        }
+
+        private void MyTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            LoadDataSet();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            var serviceRecordsList = new List<ServiceRecordObject>();
+            using(var db = new EntityModel())
+            {
+                foreach (var sr in serviceRecords)
+                {
+                    var s = db.Service.Find(sr.ServiceId);
+                    var c = db.Client.Find(sr.ClientId);
+                    TimeSpan left;
+                    Brush color = Brushes.Black;
+                    if (sr.ServiceStart >= DateTime.Now)
+                    {
+                        left = DateTime.Now - sr.ServiceStart;
+                        if(left.TotalMilliseconds < 3600000)
+                        {
+                            color = Brushes.Red;
+                        }
+                    }
+                    else
+                        left = new TimeSpan();
+
+                    var sro = new ServiceRecordObject()
+                    {
+                        ServiceRecord = sr,
+                        Service = s,
+                        Client = c,
+                        FIO = $"{c.LastName} {c.FirstName} {c.Patronymic}",
+                        Time = $"{left.Hours} часов {left.Minutes} минут",
+                        TimeColor = color
+                };
+                    serviceRecordsList.Add(sro);
+                }
+
+            }
+            recordsGridPanel.ItemsSource = serviceRecordsList;
+        }
+
+        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        {
+            new StartWindow().Show();
+            this.Close();
+        }
+
+        private void ButtonServices_Click(object sender, RoutedEventArgs e)
+        {
+            new AdminServicesWindow().Show();
+            this.Close();
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            flagNearRecords = true;
+            LoadDataSet();
+            LoadData();
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            flagNearRecords = false;
+            LoadDataSet();
+            LoadData();
         }
     }
 }
